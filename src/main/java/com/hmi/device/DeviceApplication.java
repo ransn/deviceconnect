5010d34809bd23c0b198491ad6a84f184bfbb919
Ranganath SN <ranganath.sn@sap.com>
2024-06-15 21:29:35 +0530

    DESC: Request for registers

--- src/main/java/com/hmi/device/DeviceApplication.java
+++ src/main/java/com/hmi/device/DeviceApplication.java
@@ -1,9 +1,13 @@
 package com.hmi.device;
 
+import net.wimpi.modbus.ModbusException;
 import net.wimpi.modbus.io.ModbusTCPTransaction;
 import net.wimpi.modbus.msg.ReadInputDiscretesRequest;
 import net.wimpi.modbus.msg.ReadInputDiscretesResponse;
+import net.wimpi.modbus.msg.ReadInputRegistersRequest;
+import net.wimpi.modbus.msg.ReadInputRegistersResponse;
 import net.wimpi.modbus.net.TCPMasterConnection;
+import net.wimpi.modbus.procimg.InputRegister;
 import net.wimpi.modbus.util.BitVector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -34,30 +38,48 @@ public class DeviceApplication {
 			ModbusTCPTransaction transaction = new ModbusTCPTransaction(connection);
 
 			// Prepare a Modbus request
-			ReadInputDiscretesRequest request = new ReadInputDiscretesRequest(0, 10);  // Read 10 discrete inputs starting from address 0
+			//ReadInputDiscretesRequest request = new ReadInputDiscretesRequest(0, 10);  // Read 10 discrete inputs starting from address 0
 
-			// Set the transaction request
-			transaction.setRequest(request);
+			readInputRegistersRequest(transaction);
 
+			//transaction.setRequest(request);
 			// Execute the transaction
-			transaction.execute();
+			//transaction.execute();
 
 			// Get the response
-			if (transaction.getResponse() instanceof ReadInputDiscretesResponse) {
-				ReadInputDiscretesResponse response = (ReadInputDiscretesResponse) transaction.getResponse();
-				BitVector inputs = response.getDiscretes();
-				// Process the response (print or manipulate the data as needed)
-				for (int i = 0; i < inputs.size(); i++) {
-					boolean state = inputs.getBit(i);
-					System.out.println("Discrete Input " + i + " Value: " + state);
-				}
-			}
+//			if (transaction.getResponse() instanceof ReadInputDiscretesResponse) {
+//				ReadInputDiscretesResponse response = (ReadInputDiscretesResponse) transaction.getResponse();
+//				BitVector inputs = response.getDiscretes();
+//				// Process the response (print or manipulate the data as needed)
+//				for (int i = 0; i < inputs.size(); i++) {
+//					boolean state = inputs.getBit(i);
+//					System.out.println("Discrete Input " + i + " Value: " + state);
+//				}
+//			}
 			System.out.println("Closing connection...");
 			// Close the connection
 			connection.close();
 
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		}
+	}
+
+	private static void readInputRegistersRequest(ModbusTCPTransaction transaction) throws ModbusException {
+		ReadInputRegistersRequest request = new ReadInputRegistersRequest(0,10);
+		request.setDataLength(10);
+		// Set the transaction request
+		transaction.setRequest(request);
+		// Execute the transaction
+		transaction.execute();
+		if (transaction.getResponse() instanceof ReadInputRegistersResponse) {
+			ReadInputRegistersResponse response = (ReadInputRegistersResponse) transaction.getResponse();
+			InputRegister[] inputs = response.getRegisters();
+			// Process the response (print or manipulate the data as needed)
+			for (int i = 0; i < inputs.length; i++) {
+				InputRegister register = inputs[i];
+				System.out.println("Register Input " + i + " Value: " + register.getValue());
+			}
 		}
 	}
 
